3800b7a7745ab40c3b90662b868b7c7239b8d43a
Morozov Roman <you@example.com>
2020-01-21 01:03:04 +0700

    Commit 2

--- 5/Solution_daypr.java
+++ 5/Solution_daypr.java
@@ -13,28 +13,7 @@ public class Solution {
   
     
 	static String func1(int year) {
-        String a = null;
-        
-        
-        if ((year > 1918) && (year != 1918) ) {
-            if (((year % 4 == 0)&&(year % 100 != 0)&&(year % 400!=0))||(year % 400==0)) {
-                a = "12.09." + year;
-            } else {
-                a = "13.09."+ year;
-            }
-        }else if ((year < 1918)&&(year !=1918) ) {
-            if (year % 4 == 0)  {
-                a = "12.09."+ year;
-            } else {
-                a = "13.09."+ year;
-            }
-        }
-        
-        if (year==1918)
-            a = "26.09."+ year;
-        
-        return  a;
-    }
+          }
 
 
 
